b256d50213443e601ae76d18b10d5a7e7bbffdfd
JorgeJola <jorgeandres062001@hotmail.com>
2025-04-27 14:42:38 -0500

    1

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, Flask, render_template,request, redirect, url_for, flash
+
+main=Blueprint('main',__name__)
+
+@main.route("/")
+def index():
+    return render_template('main_interface.html')
+# Temperatura
+@main.route("/temp")
+def temp():
+    return render_template('temperature.html')
+# Radiación
+@main.route("/rad")
+def rad():
+    return render_template('radiation.html')
+# Humedad
+@main.route("/hum")
+def hum():
+    return render_template('humidity.html')
+# Viento
+@main.route("/wind")
+def wind():
+    return render_template('wind.html')
